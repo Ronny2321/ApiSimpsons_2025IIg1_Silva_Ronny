ffe6b2b4d93076e180aced761e718bf56959c877
Ronny2321 <ro.silva@udla.edu.co>
2025-10-13 00:06:40 -0500

    Se ha agregado el icono de Los Simpson y el nombre al index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import './App.css'
-import Main from './Components/Main/Main'
-import NavBar from './Components/NavBar/NavBar'
-import Header from './Components/Header/Header'
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+import Header from "./Components/Header/Header";
 
-import CharactersPage from './Pages/Characters/CharactersPage'
-import LocationsPage from './Pages/Locations/LocationsPage'
-import EpisodesPage from './Pages/Episodes/EpisodesPage'
+import CharactersPage from "./Pages/Characters/CharactersPage";
+import LocationsPage from "./Pages/Locations/LocationsPage";
+import EpisodesPage from "./Pages/Episodes/EpisodesPage";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <Router>
       <Header />
-      <div id='container-body'>
+      <div id="container-body">
         <NavBar />
         <Routes>
-          <Route path='/characters' element={<CharactersPage />} />
-          <Route path='/locations' element={<LocationsPage />} />
-          <Route path='/episodes' element={<EpisodesPage />} />
+          <Route path="/characters" element={<CharactersPage />} />
+          <Route path="/locations" element={<LocationsPage />} />
+          <Route path="/episodes" element={<EpisodesPage />} />
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
