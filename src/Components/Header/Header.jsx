7e008dd8215a31d0193ec2b37c481af59ac4366d
Ronny2321 <ro.silva@udla.edu.co>
2025-10-14 02:37:18 -0500

    Se agregó animacion de nubes en el Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,17 +3,26 @@ import { NavLink } from "react-router-dom";
 import ImageTitleTheSimpsons from "../../assets/ImageTitleTheSimpsons.png";
 import Simpfamily from "../../assets/familia.ico";
 import "./Header.css";
+import CloudLeft from "../../assets/cloud-left.png";
+import CloudRight from "../../assets/cloud-right.png";
 
 const Header = () => {
   return (
     <header className="site-header">
-      <img
-        src={ImageTitleTheSimpsons}
-        alt="The Simpsons"
-        className="brand-title"
-      />
-      <h1 className="home-title">BIENVEN!DOH!</h1>
-      {/* <nav className="quick-tabs" aria-label="Secciones rápidas">
+      <div className="header-sky" aria-hidden>
+        <img src={CloudLeft} alt="" className="cloud-header cloud1" />
+        <img src={CloudRight} alt="" className="cloud-header cloud2" />
+        <img src={CloudLeft} alt="" className="cloud-header cloud3" />
+        <img src={CloudRight} alt="" className="cloud-header cloud4" />
+      </div>
+      <div className="header-inner">
+        <img
+          src={ImageTitleTheSimpsons}
+          alt="The Simpsons"
+          className="brand-title"
+        />
+        <h1 className="home-title">BIENVEN!DOH!</h1>
+        {/* <nav className="quick-tabs" aria-label="Secciones rápidas">
         <NavLink to="/" className="qt-link">
           Inicio
         </NavLink>
@@ -28,7 +37,8 @@ const Header = () => {
         </NavLink>
       </nav> */}
 
-      <img src={Simpfamily} alt="Familia Simpson" className="brand-family" />
+        <img src={Simpfamily} alt="Familia Simpson" className="brand-family" />
+      </div>
     </header>
   );
 };
